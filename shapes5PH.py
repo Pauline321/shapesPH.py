f66af880c429ffb1a0cb32739603500dd1587593
csPython <cspython@AD25TF04NGF1J-114.local>
2019-02-15 09:40:59 -0500

    moved triangle to corner of window

--- shapes5PH.py
+++ shapes5PH.py
@@ -1,11 +1,18 @@
 #shapes5PH.py by Pauline Harrell
 from graphics import*
-bTriWin = GraphWin("Blue Triangle", 300, 300)
-bTriWin.setCoords (0,0,300,300)
+bTriWin = GraphWin("Blue Triangle", 700, 700)
+bTriWin.setCoords (90,90,900,900)
 
 bTri = Polygon(Point(100,100),Point (150,200),Point(200,100))
 bTri.setFill(color_rgb(30,30,230))
 bTri.draw(bTriWin)
+
+gCircle = Circle(Point(865,865),30.5)
+gCircle.setFill(color_rgb(30,230,30))
+gCircle.draw(bTriWin)
+
+
+
 
 bTnWin.getMouse()
 bTriWin.close()
